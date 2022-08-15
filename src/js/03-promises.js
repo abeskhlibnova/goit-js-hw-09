532784b109af9ac1429387584a42dc936362e1b8
Alona Beskhlibnova <a.beskhlibnova@gmail.com>
2022-08-15 21:46:33 +0300

    hw-9 ver 1.0

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,25 +2,54 @@ import Notiflix from 'notiflix';
 
 const refs = {
   form: document.querySelector(".form"),
-  delay: document.querySelector('input[name="delay"]'),
-  step: document.querySelector('input[name="step"]'),
-  amount: document.querySelector('input[name="amount"]'),
+  delayInput: document.querySelector('input[name="delay"]'),
+  stepInput: document.querySelector('input[name="step"]'),
+  amountInput: document.querySelector('input[name="amount"]'),
   btn: document.querySelector("button"),
 }
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+let position = 0;
+
+refs.form.addEventListener("submit", timeout);
+
+function timeout (e){
+    e.preventDefault();
+    setTimeout(() => {
+    interval();
+  }, refs.delayInput.value)
 }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+function interval() {
+  position = 0;
+  setInterval(() => {
+  position += 1;
+  if (position > refs.amountInput.value) {
+      return;
+    }
+  createPromise(position, refs.delayInput.value)
+    .then(ok)
+    .catch(notOk);
+  }, refs.stepInput.value);
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+;
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    resolve(`✅ Fulfilled promise ${position} in ${delay} ms`);
+  } else {
+    reject(`❌ Rejected promise ${position} in ${delay} ms`);
+  }
+    })
+  }
+    
+  function ok(result) {
+    Notiflix.Notify.success(result);
+  }
+  function notOk(error) {
+    Notiflix.Notify.failure(error);
+  }
+
+
+
